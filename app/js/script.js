4e2a522f5084cec7faa378c55f6808adb43bfb45
md5dalton <md5daltonggs@gmail.com>
2022-05-28 03:20:49 -0400

    Toggle functionality done

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,50 @@
-console.log("hello world")
+
+const setTheme = theme => {
+    
+    localStorage.setItem("theme", theme)
+            
+    document.body.classList.add(theme)
+
+}
+
+const getTheme = () => localStorage.getItem("theme")
+
+const toggleTheme = (st) => {
+
+    const theme = getTheme()
+    
+    document.body.classList.remove(theme)
+
+    setTheme(st ? "dark" : "light")
+
+}
+
+window.onload = () => {
+
+    const toggleButton = document.querySelector(".toggle input")
+
+    const theme = getTheme()
+
+    const body = document.body
+
+    body.classList.add(theme || "light")
+
+    if (theme == "dark") toggleButton.checked = true
+    
+    toggleButton.onchange = () => toggleTheme(toggleButton.checked)
+
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").onchange = evt => {
+
+    if (evt.matches) {// dark mode
+
+        setTheme("dark")
+        
+    } else {// light mode
+        
+        setTheme("light")
+
+    }
+
+}
